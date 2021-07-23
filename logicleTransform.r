92033d4b7c3b6a3dd8ea49085f6c598a67a70ba0
Matt Hodgman <matthodg1@gmail.com>
2021-07-23 11:26:17 -0600

    correctly name output file

--- logicleTransform.r
+++ logicleTransform.r
@@ -5,4 +5,4 @@ cols <- colnames(data)
 cols <- cols[cols != 'CellID'] # do not logicle transform cell IDs
 logicleTrans <- estimateLogicle(data, cols) # automatically estimate the logicle transformation based on the data
 data <- transform(data, logicleTrans) # apply logicle transformation
-write.table(exprs(data),file=paste('logicle', args[1], sep='-'), row.names=FALSE, quote=FALSE, sep=',') # write data to csv
+write.table(exprs(data),file=paste(args[1], 'logicle', sep='.'), row.names=FALSE, quote=FALSE, sep=',') # write data to csv
